d533530852f341fa564a0fda5dcaef20e0310739
Anthony DeRobertis <anthony@derobert.net>
2010-01-24 02:40:08 -0500

    This still needs quite a bit of work. Spent wayyyy too long fighting to try and get images to load from EET file. It doesn't work, there is some special way to save images in the EET.
    And, of course, the error message is just "general error". RIGHT.
    
    Its clear I need to rebuild all of EFL with -O0 -g so that I can trace
    through everytime something doesn't work. (And I have to rebuild them,
    because otherwise the source path is wrong...)

--- pkgs/neuros-link-launcher/src/main.c
+++ pkgs/neuros-link-launcher/src/main.c
@@ -16,9 +16,10 @@ typedef struct {
 	const char *cmd;
 } LauncherButton;
 
-static const LauncherButton launcher_items[] = {
-	{ "tv", "iceweasel" },
-	{ "movie", "/opt/xbmc/bin/xbmc" }
+static const LauncherButton g_launcher_buttons[] = {
+	{ "obj/tv.png", "iceweasel" },
+	{ "obj/movie.png", "/opt/xbmc/bin/xbmc" },
+	{ "obj/chicken.png", "e16" },
 };
 
 void fatal(const char *err) {
@@ -103,6 +104,50 @@ char *find_wallpaper() {
 	return NULL;
 }
 
+void make_buttons(Evas *evas, int screen_w, int screen_h) {
+	int button_count = sizeof(g_launcher_buttons) / sizeof(LauncherButton);
+
+	int usable_w = screen_w * 3/5;
+	int usable_h = screen_h * 3/5;
+
+	int button_size, left_usable, top_usable;
+	if (usable_w / button_count > usable_h) {
+		// we are height constrained, so set size to usable height and
+		// center horizontally
+		button_size = usable_h;
+		left_usable = (usable_w - (button_size * button_count)) / 2;
+		top_usable = 0;
+	} else {
+		// we are width constrained (normal case), so set size to usable
+		// width divided by button count and center vertically.
+		button_size = usable_w / button_count;
+		left_usable = 0;
+		top_usable = (usable_h - button_size) / 2;
+	}
+
+	int left_absolute = left_usable + (screen_w * 1/5);
+	int top_absolute = top_usable + (screen_h * 1/5);
+
+	for (int i = 0; i < button_count; ++i) {
+		Evas_Object *btn = evas_object_rectangle_add(evas);
+		evas_object_resize(btn, button_size, button_size);
+		evas_object_move(btn, left_absolute, top_absolute);
+		evas_object_color_set(btn, 0, 0, 0, 100);
+		evas_object_show(btn);
+
+		Evas_Object *btni = evas_object_image_filled_add(evas);
+		evas_object_image_file_set(btni, g_launcher_buttons[i].icon, "");
+		printf("btni->load_error: %i\n", evas_object_image_load_error_get(btni));
+		evas_object_image_smooth_scale_set(btni, 1);
+		evas_object_resize(btni, button_size, button_size);
+		evas_object_move(btni, left_absolute, top_absolute);
+		evas_object_show(btni);
+
+
+		left_absolute += button_size;
+	}
+}
+
 int main(int argc, char **argv) {
     Ecore_Evas *ee;
     Evas *evas;
@@ -110,6 +155,10 @@ int main(int argc, char **argv) {
     init_efl();
 
     ee = ecore_evas_software_x11_new(NULL, 0,  0, 0, 400, 400);
+    if (!ee) {
+		fprintf(stderr, "FATAL: Could not get an ee...\n");
+		exit(1);
+	}
     Ecore_X_Screen_Size sz = get_screen_size(ee);
     ecore_evas_resize(ee, sz.width, sz.height);
     ecore_evas_title_set(ee, "Launcher");
@@ -121,14 +170,16 @@ int main(int argc, char **argv) {
 
 	char *wallpaper;
 	if ((wallpaper = find_wallpaper())) {
-		Evas_Object *bg = evas_object_image_add(evas);
+		Evas_Object *bg = evas_object_image_filled_add(evas);
 		evas_object_image_file_set(bg, wallpaper, "");
+		evas_object_image_smooth_scale_set(bg, 1);
 		evas_object_move(bg, 0, 0);
 		evas_object_resize(bg, sz.width, sz.height);
-		evas_object_image_fill_set(bg, 0, 0, sz.width, sz.height);
 		evas_object_show(bg);
 		free(wallpaper);
 	}
+
+	make_buttons(evas, sz.width, sz.height);
 
     ecore_evas_show(ee);
     ecore_main_loop_begin();
